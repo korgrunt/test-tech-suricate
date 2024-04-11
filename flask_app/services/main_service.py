86f583b2be2ca813fc579be625d1c1395d3dc088
korgrunt <naim.aouaichia@gmail.com>
2024-04-12 00:52:25 +0200

    modular python flask api -- use docker compose only for oddoo an prestshop

---
+++ flask_app/services/main_service.py
@@ -0,0 +1,12 @@
+
+class ProductService:
+    def get_prestashop_products(self):
+        return ['presta_User1', 'presta_User2', 'presta_User3']
+    
+    def get_odoo_products(self):    
+        return ['odoo_User1', 'odoo_User2', 'odoo_User3']
+    
+        
+    def get_products(self):    
+        return self.get_odoo_products() + self.get_prestashop_products()
+
