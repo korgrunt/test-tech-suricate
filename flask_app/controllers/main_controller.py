29f2152b575e3acbd33fd810130e52fc822cd502
korgrunt <naim.aouaichia@gmail.com>
2024-04-12 15:27:20 +0200

    creat odoo rpc-client for list and create product

--- flask_app/controllers/main_controller.py
+++ flask_app/controllers/main_controller.py
@@ -23,3 +23,21 @@ def get_products():
     if(products_sources  == "prestashop"):
         return service.get_prestashop_products()
     return "Unknown source", 400
+
+@main_controller.route('/products', methods=['POST'])
+def post_product():
+    products_target = request.args.get('target')
+    body = request.get_json()
+
+        # Vérifier si les champs requis existent dans les données
+    if 'name' not in body or 'price' not in body or 'description' not in body:
+        # Récupérer les valeurs des champs
+        return "you mut pecify name, price and description in request body", 400
+    if products_target is None:
+        return "You must specify a target", 400
+    if(products_target  == "odoo"):
+        return service.create_odoo_products(body['name'], body['price'], body['description'])
+    if(products_target  == "prestashop"):
+        return service.get_prestashop_products()
+    return "Unknown source", 400
+
--- flask_app/services/main_service.py
+++ flask_app/services/main_service.py
@@ -1,49 +1,21 @@
-# services
-import xmlrpc.client
-from dotenv import load_dotenv
-import os
-
-# Informations de connexion à votre instance Odoo
-url = os.getenv('ODOO_URL')
-db = os.getenv('ODOO_DB_NAME')
-username = os.getenv('ODOO_ADMIN_USERNAME')
-password = os.getenv('ODOO_ADMIN_PASSWORD')
-
-# Connexion à l'instance Odoo
-common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
-uid = common.authenticate(db, username, password, {})
+from clients.odoo_client import OdooClient
 
 
 class ProductService:
+    def __init__(self):
+        self.odooClient = OdooClient()
+        
     def get_prestashop_products(self):
         return ['presta_User1', 'presta_User2', 'presta_User3']
     
     def get_odoo_products(self):
-        # Création d'une nouvelle connexion pour l'API
-        models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
-
-        # Recherche des produits et récupération de leurs informations
-        product_ids = models.execute_kw(db, uid, password,
-            'product.product', 'search', [[]])  # Récupérer les 10 premiers produits par exemple
-
-        products = models.execute_kw(db, uid, password,
-            'product.product', 'read', [product_ids], {'fields': ['name', 'list_price', 'description']})
-
-        # Affichage des informations des produits
-        for product in products:
-            print("Nom du produit:", product['name'])
-            print("Prix du produit:", product['list_price'])
-            print("Description du produit:", product['description'])
-            print("-----------------------------------")
-        
-        return products
+        return self.odooClient.get_products()
     
     def create_prestashop_products(self):
         return ['presta_User1', 'presta_User2', 'presta_User3']
     
-    def create_odoo_products(self):    
-        return ['odoo_User1', 'odoo_User2', 'odoo_User3']
-    
+    def create_odoo_products(self, name, price, description):    
+        return self.odooClient.create_products(name, price, description)
         
     def get_products(self):    
         return self.get_odoo_products() + self.get_prestashop_products()
