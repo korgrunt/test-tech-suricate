80f5061ed7a6e14a43dd20e394947e071c760923
korgrunt <naim.aouaichia@gmail.com>
2024-04-14 22:05:37 +0200

    Prestashop client implemented - api ready for be used as connector in simple replaction way for productio fom pretasop to odoo and from odoo to pretashop

--- flask_app/services/main_service.py
+++ flask_app/services/main_service.py
@@ -1,9 +1,10 @@
 from clients.odoo_client import OdooClient
-
+from clients.prestashop_client import PrestaShopClient
 
 class ProductService:
     def __init__(self):
         self.odooClient = OdooClient()
+        self.prestashopClient = PrestaShopClient()
         
     def get_prestashop_products(self):
         return ['presta_User1', 'presta_User2', 'presta_User3']
@@ -11,8 +12,8 @@ class ProductService:
     def get_odoo_products(self):
         return self.odooClient.get_products()
     
-    def create_prestashop_products(self):
-        return ['presta_User1', 'presta_User2', 'presta_User3']
+    def create_prestashop_products(self, name, price, description):
+        return self.prestashopClient.create_products(name, price, description)
     
     def create_odoo_products(self, name, price, description):    
         return self.odooClient.create_products(name, price, description)
